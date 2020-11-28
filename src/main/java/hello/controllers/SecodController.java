85b3017376725e3ee2411021994bfd245a084b2d
Vasypu <lesch.kostenko@yandex.ru>
2020-11-28 03:54:40 +0300

    forth lesson

--- src/main/java/hello/controllers/SecodController.java
+++ src/main/java/hello/controllers/SecodController.java
@@ -1,13 +1,34 @@
 package hello.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SecodController {
 
     @GetMapping("/exit")
-    public String exit() {
+    public String exit(@RequestParam(value = "a") int a,
+                       @RequestParam(value = "b") int b,
+                       @RequestParam(value = "action", required = false) String action,
+                       Model model) {
+        int result;
+
+        switch (action) {
+            case "multiplication": result = a*b;
+            break;
+            case "addition": result = a+b;
+            break;
+            case "subtraction": result = a-b;
+            break;
+            case "division": result = a/b;
+            break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + action);
+        }
+
+        model.addAttribute("message", "result= " + result);
         return "second/exit";
     }
 }
