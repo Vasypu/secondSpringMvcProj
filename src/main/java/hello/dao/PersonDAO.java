95051c89b0633de943b0263f713774315e7aa831
Vasypu <lesch.kostenko@yandex.ru>
2020-11-30 00:17:56 +0300

    fifth lesson

---
+++ src/main/java/hello/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package hello.dao;
+
+import hello.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(1, "Tom"));
+        people.add(new Person(2, "Bob"));
+        people.add(new Person(3, "Mike"));
+        people.add(new Person(3, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
