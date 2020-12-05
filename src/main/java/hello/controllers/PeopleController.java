3180b5b4db99bbd6b831ca25ed5a592927a07b60
Vasypu <lesch.kostenko@yandex.ru>
2020-12-05 03:02:28 +0300

    six lesson

--- src/main/java/hello/controllers/PeopleController.java
+++ src/main/java/hello/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package hello.controllers;
 
 import hello.dao.PersonDAO;
+import hello.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -25,5 +24,17 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+//        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/hello/dao/PersonDAO.java
+++ src/main/java/hello/dao/PersonDAO.java
@@ -9,15 +9,16 @@ import java.util.List;
 @Component
 public class PersonDAO {
 
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person(1, "Tom"));
-        people.add(new Person(2, "Bob"));
-        people.add(new Person(3, "Mike"));
-        people.add(new Person(3, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
     public List<Person> index() {
@@ -26,5 +27,10 @@ public class PersonDAO {
 
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
