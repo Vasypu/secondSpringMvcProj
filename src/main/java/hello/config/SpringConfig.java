b00b98185767c91cd8ef2c528d28cb5e4e95ec05
Vasypu <lesch.kostenko@yandex.ru>
2020-12-06 19:02:30 +0300

    seven lesson

--- src/main/java/hello/config/SpringConfig.java
+++ src/main/java/hello/config/SpringConfig.java
@@ -6,12 +6,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @Configuration
 @ComponentScan("hello")
