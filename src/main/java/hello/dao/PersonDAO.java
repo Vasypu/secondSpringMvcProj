c5c988a6b4b36a05f4e67137a32576a91a81b955
Vasypu <lesch.kostenko@yandex.ru>
2021-01-06 01:17:22 +0300

    twelve lesson

--- src/main/java/hello/dao/PersonDAO.java
+++ src/main/java/hello/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package hello.dao;
 
 import hello.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import java.sql.*;
@@ -19,11 +20,11 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM person", new PersonMapper());
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM person WHERE id=?", new Object[]{id}, new PersonMapper())
+        return jdbcTemplate.query("SELECT * FROM person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
